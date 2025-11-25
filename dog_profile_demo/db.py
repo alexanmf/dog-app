40c0d51bf73f6155cf0947bbe776d9ab731ee4b6
alexanmf <125303579+alexanmf@users.noreply.github.com>
2025-11-24 21:50:04 -0500

    Switch database connection from SQLite to PostgreSQL

--- dog_profile_demo/db.py
+++ dog_profile_demo/db.py
@@ -1,16 +1,16 @@
-import sqlite3
+import os
 from flask import g
+import psycopg2
+import psycopg2.extras
 
-DATABASE = "dogs.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
+    if "db" not in g:
+        g.db = psycopg2.connect(DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+    return g.db
 
 def close_db(e=None):
-    db = getattr(g, '_database', None)
+    db = g.pop("db", None)
     if db is not None:
         db.close()
