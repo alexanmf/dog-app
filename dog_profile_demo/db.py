0593aa5b74d3a2186f096b073948845e94be25d7
alexanmf <125303579+alexanmf@users.noreply.github.com>
2026-04-15 21:58:11 -0400

    Refactor db initialization in db.py
    Removed docstring and unused context for db initialization.

--- dog_profile_demo/db.py
+++ dog_profile_demo/db.py
@@ -2,14 +2,7 @@ from models import db
 
 
 def init_db(app):
-    """
-    Initialize SQLAlchemy with the Flask app and create all tables.
-    """
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
 
 
 
